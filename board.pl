49f55156db372f6d3310934522d1bc5a35050122
Diogo Campos <d.diogocampos@gmail.com>
2016-10-13 00:22:53 +0100

    Deleted blank lines

--- board.pl
+++ board.pl
@@ -11,9 +11,7 @@ board([[o, o, o, o, o, o, o, o, o],
 translate(o) :- write(' ').
 
 displayLine([]) :- nl.
-
 displayLine([X|Xs]) :- translate(X), write('|'), displayLine(Xs).
 
 displayBoard([]) :- nl.
-
 displayBoard([L|Ls]) :- displayLine(L), nl, displayBoard(Ls).
