0bc261716affcf802f65a823ce6ae2581eb6e0e6
williamnf <wnfukunaga@gmail.com>
2016-10-12 23:49:53 +0100

    displaying raw board

--- board.pl
+++ board.pl
@@ -7,3 +7,13 @@ board([[o, o, o, o, o, o, o, o, o],
     [o, o, o, o, o, o, o, o, o],
     [o, o, o, o, o, o, o, o, o],
     [o, o, o, o, o, o, o, o, o]]).
+
+translate(o) :- write(' ').
+
+displayLine([]) :- nl..
+
+displayLine([X|Xs]) :- translate(X), write('|'), displayLine(Xs).
+
+displayBoard([]) :- nl.
+
+displayBoard([L|Ls]) :- displayLine(L), nl, displayBoard(Ls).
