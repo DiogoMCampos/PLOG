8dfb9f26ad5eacafecc9273e9427d7b5ff083003
williamnf <wnfukunaga@gmail.com>
2016-10-13 11:03:52 +0100

    displaying midGame

--- board.pl
+++ board.pl
@@ -18,6 +18,16 @@ boardStart([[r3, o, o, o, o, o, o, o, r3],
     [o, o, w2, w1, w1, w1, w2, o, o],
     [w3, o, o, o, o, o, o, o, w3]]).
 
+boardMidGame([[o, o, o, o, o, o, o, o, o],
+    [o, r1, o, o, o, o, o, o, o],
+    [o, o, o, o, r1, o, o, o, o],
+    [o, o, o, r3, o, o, o, o, o],
+    [o, o, o, o, o, o, w2, o, o],
+    [o, o, o, w3, o, r1, o, o, o],
+    [o, o, r3, o, o, o, o, o, o],
+    [o, o, o, o, o, w3, w1, r2, r2],
+    [o, o, w1, w1, o, o, o, o, w2]]).
+
 translate(o) :- write('  ').
 translate(w3) :- write('W3').
 translate(w2) :- write('W2').
@@ -28,6 +38,8 @@ translate(r1) :- write('R1').
 
 game(X) :- board(X), displayBoard(X).
 start(X) :- boardStart(X), displayBoard(X).
+midGame(X) :- boardMidGame(X), displayBoard(X).
+
 displayLine([]) :- write('|'), nl.
 displayLine([X|Xs]) :- write('|'),translate(X),  displayLine(Xs).
 
