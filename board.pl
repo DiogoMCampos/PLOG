3054ce1fd04fb5478f83994d68e7a40bf786b934
williamnf <wnfukunaga@gmail.com>
2016-10-13 10:46:35 +0100

    start of the game now displaying

--- board.pl
+++ board.pl
@@ -8,9 +8,26 @@ board([[o, o, o, o, o, o, o, o, o],
     [o, o, o, o, o, o, o, o, o],
     [o, o, o, o, o, o, o, o, o]]).
 
-translate(o) :- write(' ').
+boardStart([[r3, o, o, o, o, o, o, o, r3],
+    [o, o, r2, r1, r1, r1, r2, o, o],
+    [o, o, o, o, r1, o, o, o, o],
+    [o, o, o, o, o, o, o, o, o],
+    [o, o, o, o, o, o, o, o, o],
+    [o, o, o, o, o, o, o, o, o],
+    [o, o, o, o, w1, o, o, o, o],
+    [o, o, w2, w1, w1, w1, w2, o, o],
+    [w3, o, o, o, o, o, o, o, w3]]).
+
+translate(o) :- write('  ').
+translate(w3) :- write('W3').
+translate(w2) :- write('W2').
+translate(w1) :- write('W1').
+translate(r3) :- write('R3').
+translate(r2) :- write('R2').
+translate(r1) :- write('R1').
 
 game(X) :- board(X), displayBoard(X).
+start(X) :- boardStart(X), displayBoard(X).
 displayLine([]) :- write('|'), nl.
 displayLine([X|Xs]) :- write('|'),translate(X),  displayLine(Xs).
 
