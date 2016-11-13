7384472f51ff6b4bdf3438fe79aebd7d9dd434c0
Diogo Campos <d.diogocampos@gmail.com>
2016-11-13 23:45:03 +0000

    Added utilities.pl file

--- board.pl
+++ board.pl
@@ -1,48 +1,9 @@
 :-use_module(library(lists)).
 :-use_module(library(random)).
 :-include('interface.pl').
+:-include('utilities.pl').
 
 boardStartIndex(1).
-
-convertLetterToIndex(Column, [X|Xs], Index, Result) :-
-    Column \== X ->
-        NewIndex is Index+1,
-        convertLetterToIndex(Column, Xs, NewIndex, Result);
-    returnResult(Index, Result).
-
-withinBoard(X, Y, Size) :-
-    X > 0,
-    X =< Size,
-    Y > 0,
-    Y =< Size.
-
-isOrthogonal(InC, InL, DeC, DeL, HorMove, VertMove, Amount) :-
-    (InC == DeC ->
-        HorMove is 0,
-        (InL > DeL ->
-            Amount is InL - DeL,
-            VertMove is -1
-        ;   InL < DeL ->
-                Amount is DeL - InL,
-                VertMove is 1))
-    ;   (InL == DeL ->
-        VertMove is 0,
-        (InC > DeC ->
-            Amount is InC - DeC,
-            HorMove is -1
-        ;   InC < DeC ->
-                Amount is DeC - InC,
-                HorMove is 1)).
-
-returnResult(Result, Result).
-
-getListElement(Index, [X|Xs], Iterator, Result) :-
-    Index =\= Iterator ->
-        NewIterator is Iterator+1,
-        getListElement(Index, Xs, NewIterator, Result)
-    ;   returnResult(X,Result).
-
-isPiece(piece(_,_)).
 
 getPiece([X|Xs], ColumnIndex, LineIndex, Piece) :-
     getListElement(10-LineIndex, [X|Xs], 1, Line),
@@ -94,10 +55,6 @@ getPiecesCoordinates(Board, Column, Line, Side, [PieceCoords|Locals], PiecesLeft
         ;   getPiecesCoordinates(Board, NewCol, NewLine, Side, [PieceCoords|Locals], PiecesLeft, PiecesTotal))
     ;   returnResult(PiecesLeft, PiecesTotal)).
 
-getFirstValuePair(Value-_, Value).
-getSecondValuePair(_-Value, Value).
-
-addToList(Element, List, [Element|List]).
 
 getOutsideBoard([], _).
 getOutsideBoard([Pair|PairRest], [Removed|RemovedRest]) :-
